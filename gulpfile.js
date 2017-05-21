7c34b81670fce2aae29afe1f915ccaadd3f5d52d
Davide Vico <davide.vico@alice.it>
2017-05-21 16:14:06 +0200

    added gulp

---
+++ gulpfile.js
@@ -0,0 +1,95 @@
+const bs = require('browser-sync').create();
+const sass = require('gulp-sass');
+const gulp = require('gulp');
+const plumber = require('gulp-plumber');
+const beep = require('beepbeep');
+const pug = require('gulp-pug');
+const uglify = require('gulp-uglify');
+const babel = require('gulp-babel');
+const concat = require('gulp-concat');
+const uglifyCSS = require('gulp-uglifycss');
+const rename = require('gulp-rename');
+const uncss = require('gulp-uncss');
+
+'use strict';
+
+const errorHandler = (err) => {
+  console.log('ERROR', err.message);
+  console.log('CAUSE', err.cause);
+  console.log(err);
+  bs.notify( `Error in ${err.relativePath}:${err.line}`, 5000);
+  beep();
+}
+const APPNAME = 'musicsearch';
+const paths = {
+  sass: [ './sass/main.sass', './sass/*.sass', './www/components/**/*.sass' ],
+  pug: [ './www/index.pug', './www/components/**/*.pug' ],
+  html: [ './www/index.html', './www/components/**/*.html' ],
+  scripts: [ './www/js/**/*.js', '!./www/js/external/*' ],
+  barrels: {},
+  dist: {
+    scripts: './www/dist/',
+    styles: './www/dist/'
+  }
+};
+paths.barrels.sass = paths.sass[0];
+
+const emptyFn = function(){};
+const compilePug = ( target, cb = emptyFn ) => {
+  return gulp.src(target)
+    .pipe(plumber({ errorHandler }))
+    .pipe(pug({ pretty: true }))
+    // compile file in the origin folder
+    .pipe(gulp.dest( (file) => file.base ))
+    .on('end', cb);
+}
+
+const onPugChange = (e) => {
+  // converting absolute in relative path
+  // the base dir is ./www, removing everything before
+  const relPath = e.path.slice( e.path.indexOf('www') );
+
+  console.log(`pug: ${ relPath } updated`);
+  compilePug(relPath, bs.reload);
+}
+
+gulp.task('serve', ['sass'], function(){
+    bs.init({ server: './www/' });
+    gulp.watch(paths.sass, ['sass']);
+    gulp.watch(paths.pug, onPugChange);
+});
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function(){
+   gulp.src(paths.barrels.sass)
+    .pipe(plumber({ errorHandler }))
+    .pipe(sass())
+    .pipe(gulp.dest('./www/'))
+    .pipe(bs.stream());
+});
+
+gulp.task('pug', function() {
+  compilePug( paths.pug );
+});
+
+gulp.task('scripts-prod', function(){
+  gulp.src(paths.scripts)
+    .pipe(plumber({ errorHandler }))
+    .pipe(babel())
+    .pipe(concat(`${APPNAME}.bundle.js`))
+    .pipe(uglify())
+    .pipe(gulp.dest(paths.dist.scripts));
+});
+
+gulp.task('styles-prod', ['sass'], function(){
+  gulp.src(`./www/main.css`)
+    .pipe(uncss({ html: paths.html }))
+    .pipe(plumber({ errorHandler }))
+    .pipe(rename(`${APPNAME}.min.css`))
+    .pipe(uglifyCSS())
+    .pipe(gulp.dest(paths.dist.styles));
+});
+
+gulp.task('build', ['scripts-prod', 'styles-prod']);
+
+gulp.task('default', ['serve']);
