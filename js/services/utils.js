cc237ea919d74aa514525ad173dfcf654a98139d
Davide Vico <davide.vico@alice.it>
2017-05-31 23:29:15 +0200

    adjusted search results

--- js/services/utils.js
+++ js/services/utils.js
@@ -8,3 +8,24 @@ utils.factory('ajax', function ($http) {
 
   return { call }
 });
+
+utils.factory('arrays', function ($http) {
+  const shuffle = ( arr, exit = false ) => {
+    let i, j, tmp;
+    for( i=arr.length-1; i>0; i-- ){
+      j = Math.floor(Math.random() * (i + 1));
+      tmp = arr[i];
+      arr[i] = arr[j];
+      arr[j] = tmp;
+    }
+
+    // trying to lower probability of having elements in the same position
+    // they had before the shuffling
+    if( !exit ){ 
+      shuffle(arr, true);
+    }
+    return arr;
+  }
+
+  return { shuffle }
+});
