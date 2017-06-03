daecf0645c904104b2f8888d794d0cb5e058b692
Davide Vico <davide.vico@alice.it>
2017-06-03 19:19:42 +0200

    added spinners to improve UX

--- components/tracks/tracks.controller.js
+++ components/tracks/tracks.controller.js
@@ -3,14 +3,35 @@ angular.module('tracks', ['modalHelper'])
 .controller('tracksCtrl', function($scope, modalHelper){
 
   $scope.model = [];
+  $scope.showLoader = true;
   const _modalID = 'openTracksModal';
 
   modalHelper.on('reset', (modalId) => {
     if( _modalID === modalId ){
-      $scope.model = modalHelper.getModel();
-      $scope.model.tracks.forEach((track) => {
+      const model = modalHelper.getModel()
+      model.tracks.forEach((track) => {
         track.duration = moment.utc(track.duration_ms).format("m:ss")
       });
+      $scope.showLoader = false;
+      $scope.model = model;
+    }
+  });
+
+  modalHelper.on('showLoader', (modalId) => {
+    if( _modalID === modalId ){
+      $scope.showLoader = true;
+    }
+  });
+
+  modalHelper.on('hideLoader', (modalId) => {
+    if( _modalID === modalId ){
+      $scope.showLoader = false;
+    }
+  });
+
+  modalHelper.on('clearModel', (modalId) => {
+    if( _modalID === modalId ){
+      $scope.model = [];
     }
   });
   
